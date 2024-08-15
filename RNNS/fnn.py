ba2c81695d5dd8c6eb9d9a7d1a67b6a06b72b5a0
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-08-15 14:00:12 +0300

    initial commit

---
+++ RNNS/fnn.py
@@ -0,0 +1,14 @@
+import numpy as np
+from dataclasses import dataclass
+
+
+class RNN():
+    def __init__(self,n_layer,
+                 n_neuron,
+                 ):
+        
+        self.n_layer=n_layer
+        self.n_neuron=n_neuron
+
+    def forward():
+        
