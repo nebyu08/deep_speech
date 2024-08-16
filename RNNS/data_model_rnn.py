a9a16eedcf5e34b895735bb35f49c5189af4bc01
nebyu08 <nebiyuyouhannes08@gmail.com>
2024-08-16 15:40:59 +0300

    initial version of whole data encoding

--- RNNS/data_model_rnn.py
+++ RNNS/data_model_rnn.py
@@ -6,13 +6,24 @@ with open('./dataset/input.txt') as f:
 
 char_index={char:index for index,char in enumerate(string.ascii_lowercase)}
 
-def data_model():
+def data_model(input_string):
+    """turns string into one hot encode
+
+    Args:
+        input_string (paragraph): the input text
+
+    Returns:
+        _type_: string
+    """
+    paragraph=input_string.split('/n')
+
     index_embed=[]
-    for i in range(26):
-        char_encode=[]
-        char_encode=np.zeros(26)
-        char_encode[i]=1
-        index_embed.append(char_encode)
+    for row in range(paragraph):
+        for i in row.lower():
+            char_encode=[]
+            char_encode=np.zeros((len(string.ascii_lowercase),1))
+            char_encode[char_index[i]]=1
+            index_embed.append(char_encode)
     return index_embed  
 
 print(data_model())
